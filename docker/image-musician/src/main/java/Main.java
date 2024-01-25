400099debce27c716960b159dd9d4c7902e7d543
Fabrice Chapuis <fabrice.chapuis@heig-vd.ch>
2024-01-25 12:31:35 +0100

    Implemented UDP protocol and added maven shade to the dependencies

--- docker/image-musician/src/main/java/Main.java
+++ docker/image-musician/src/main/java/Main.java
@@ -1,9 +1,15 @@
+import java.net.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+
+import com.google.gson.Gson;
 
 class Main {
     static String instrument;
     static String sound;
+    static String uuid;
+
     public static void main(String[] args) {
         if (args.length != 1) {
             throw new IllegalArgumentException("We need exactly one argument");
@@ -18,6 +24,27 @@ class Main {
             throw new IllegalArgumentException("Unknown instrument");
         }
 
+        uuid = UUID.randomUUID().toString();
+
+        try {
+            InetAddress group = InetAddress.getByName("224.0.0.1");
+            int port = 9904;
+
+            while (true) {
+                String json = createJson();
+                byte[] buffer = json.getBytes();
+
+                System.out.println("Sending " + json);
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, port);
+                DatagramSocket socket = new DatagramSocket();
+                System.out.println("packet sent");
+                socket.send(packet);
+
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static Map<String, String> initMap() {
@@ -28,5 +55,15 @@ class Main {
         sounds.put("violin", "gzi-gzi");
         sounds.put("drum", "boum-boum");
         return sounds;
+    }
+
+    private static String createJson() {
+        Map<String, String> jsonMap = new HashMap<String, String>();
+        jsonMap.put("instrument", instrument);
+        jsonMap.put("sound", sound);
+        jsonMap.put("uuid", uuid);
+
+        Gson gson = new Gson();
+        return gson.toJson(jsonMap);
     }
 }
